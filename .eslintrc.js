296e3c34995ef3f9bfe5590e7d1b245a2553eb24
H.P.Leung <kipperlhp@yahoo.com.hk>
2019-12-05 00:27:15 +0800

    eslint config set up

---
+++ .eslintrc.js
@@ -0,0 +1,16 @@
+module.exports = {
+  "extends": [
+    "airbnb",
+  ],
+  rules: {
+    "arrow-body-style": 0,
+    "linebreak-style": ["error", "windows"],
+    "max-len": 0,
+    "object-curly-newline": 0,
+    "react/forbid-prop-types": 0,
+    "react/jsx-props-no-spreading": "off",
+    "react/jsx-filename-extension": [1, { "extensions": [".js", ".jsx"] }],
+    "react/require-default-props": 0,
+    "semi": [2, "never"],
+  },
+};
