12f7293314c54e2f6f696eb7fafeedf5429323fd
H.P.Leung <kipperlhp@yahoo.com.hk>
2019-12-14 02:02:46 +0800

    favicon.ico updated

--- gatsby-config.js
+++ gatsby-config.js
@@ -25,7 +25,7 @@ module.exports = {
         background_color: '#0F0F0F',
         theme_color: '#0F0F0F',
         display: 'standalone',
-        icon: 'src/images/icon.png', // This path is relative to the root of the site.
+        icon: 'src/images/favicon.ico', // This path is relative to the root of the site.
       },
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality
