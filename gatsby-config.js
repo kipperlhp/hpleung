663cd69018fd66e0eec7ec8044f99108a83d47f4
H.P.Leung <kipperlhp@yahoo.com.hk>
2019-12-28 18:25:27 +0800

    Sitemap & robot.txt added

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,6 +5,7 @@ module.exports = {
     description: 'H.P.Leung (Leung Hiu Pak) is a Software Developer based in Hong Kong, focusing in web application development.',
     author: 'H.P.Leung',
     image: 'https://hpleung.com/icons/icon-256x256.png',
+    siteUrl: 'https://www.hpleung.com',
   },
   plugins: [
     'gatsby-plugin-react-helmet',
@@ -29,6 +30,28 @@ module.exports = {
         theme_color: '#0F0F0F',
         display: 'standalone',
         icon: 'src/images/icon.png', // This path is relative to the root of the site.
+      },
+    },
+    {
+      resolve: 'gatsby-plugin-sitemap',
+      options: {
+        output: '/sitemap.xml',
+        exclude: [
+          '/404',
+          '/test',
+        ],
+      },
+    },
+    {
+      resolve: 'gatsby-plugin-robots-txt',
+      options: {
+        host: 'https://www.hpleung.com',
+        sitemap: 'https://www.hpleung.com/sitemap.xml',
+        policy: [{
+          userAgent: '*',
+          allow: '/',
+          disallow: ['/assets/*'],
+        }],
       },
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality
--- src/pages/test.js
+++ src/pages/test.js
@@ -5,13 +5,13 @@ import Layout from '../components/layout'
 import SEO from '../components/seo'
 import Text from '../components/atoms/Text'
 
-const SecondPage = () => (
+const TestPage = () => (
   <Layout>
-    <SEO title="Page two" />
-    <Text variant="h1">Hi from the second page</Text>
-    <p>Welcome to page 2</p>
+    <SEO title="Test Page" />
+    <Text variant="h1">Hi from the Test page</Text>
+    <p>Welcome to Test Page</p>
     <Link to="/">Go back to the homepage</Link>
   </Layout>
 )
 
-export default SecondPage
+export default TestPage
