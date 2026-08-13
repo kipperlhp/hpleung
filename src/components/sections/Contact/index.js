3c381452e1994bbc4e20c1e1d94884c626f73b41
H.P.Leung <kipperlhp@yahoo.com.hk>
2026-08-14 03:03:23 +0800

    Add Image for Contact section

--- src/components/sections/Contact/index.js
+++ src/components/sections/Contact/index.js
@@ -2,7 +2,10 @@ import React from 'react'
 import styled from 'styled-components'
 import { palette } from 'styled-theme'
 import Fade from 'react-reveal/Fade'
+import { graphql, useStaticQuery } from 'gatsby'
+import { Flex } from '@rebass/grid'
 import Text from '../../atoms/Text'
+import Image from '../../atoms/Image'
 import ScrollSection from '../../molecules/ScrollSection'
 
 const EmailText = styled(Text)`
@@ -13,17 +16,35 @@ const EmailText = styled(Text)`
 `
 
 const ContactSection = () => {
+  const data = useStaticQuery(graphql`
+    query {
+      contactImage: file(relativePath: { eq: "contact.jpg" }) {
+        childImageSharp {
+          fluid(maxWidth: 960) {
+            ...GatsbyImageSharpFluid
+          }
+        }
+      },
+    }
+  `)
   return (
     <ScrollSection name="contact" title="Contact" mb="10rem">
       <Fade bottom cascade>
-        <Text align="center">
-          Need to get in touch with me? Feel free to drop me an email!
-        </Text>
-        <a href="mailto: kipperlhp@yahoo.com.hk">
-          <EmailText bold>
-            kipperlhp@yahoo.com.hk
-          </EmailText>
-        </a>
+        <Flex flexDirection="column" alignItems="center">
+          <Image
+            src={data.contactImage.childImageSharp.fluid}
+            style={{ width: '150%', minHeight: 0, maxWidth: '90vw', marginBottom: '1.5rem' }}
+            alt="Contact Me"
+          />
+          <Text align="center">
+            Need to get in touch with me? Feel free to drop me an email!
+          </Text>
+          <a href="mailto: kipperlhp@yahoo.com.hk">
+            <EmailText bold>
+              kipperlhp@yahoo.com.hk
+            </EmailText>
+          </a>
+        </Flex>
       </Fade>
     </ScrollSection>
   )
