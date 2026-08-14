bd839f05017c968cbe697e504cba53e55b1651fd
H.P.Leung <kipperlhp@yahoo.com.hk>
2026-08-15 06:46:57 +0800

    Fix Contact sections revealing elements not cascade

--- src/components/sections/Contact/index.js
+++ src/components/sections/Contact/index.js
@@ -3,7 +3,6 @@ import styled from 'styled-components'
 import { palette } from 'styled-theme'
 import Fade from 'react-reveal/Fade'
 import { graphql, useStaticQuery } from 'gatsby'
-import { Flex } from '@rebass/grid'
 import Text from '../../atoms/Text'
 import Image from '../../atoms/Image'
 import ScrollSection from '../../molecules/ScrollSection'
@@ -30,21 +29,19 @@ const ContactSection = () => {
   return (
     <ScrollSection name="contact" title="Contact" mb="10rem">
       <Fade bottom cascade>
-        <Flex flexDirection="column" alignItems="center">
-          <Image
-            src={data.contactImage.childImageSharp.fluid}
-            style={{ width: '150%', minHeight: 0, maxWidth: '90vw', marginBottom: '1.5rem' }}
-            alt="Contact Me"
-          />
-          <Text align="center">
-            Need to get in touch with me? Feel free to drop me an email!
-          </Text>
-          <a href="mailto: kipperlhp@yahoo.com.hk">
-            <EmailText bold>
-              kipperlhp@yahoo.com.hk
-            </EmailText>
-          </a>
-        </Flex>
+        <Image
+          src={data.contactImage.childImageSharp.fluid}
+          style={{ width: 'min(90vw, 730px)', marginBottom: '1.5rem' }}
+          alt="Contact Me"
+        />
+        <Text align="center">
+          Need to get in touch with me? Feel free to drop me an email!
+        </Text>
+        <a href="mailto: kipperlhp@yahoo.com.hk">
+          <EmailText bold>
+            kipperlhp@yahoo.com.hk
+          </EmailText>
+        </a>
       </Fade>
     </ScrollSection>
   )
